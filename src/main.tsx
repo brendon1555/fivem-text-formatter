a990561521155b2ee325b6d56f7f2946ef651133
Brendon Lees <brendon1555@gmail.com>
2025-09-27 18:47:58 +1000

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { TourProvider } from './contexts/TourContext'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
